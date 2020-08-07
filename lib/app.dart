003c57a70762af14c2c82fcd10819e2bd5b36a6b
Rene <aranar@oregonstate.edu>
2020-08-07 15:54:37 -0400

    Added Sentry crash reporting.

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,32 @@
+import 'package:flutter/foundation.dart' as Foundation;
 import 'package:flutter/material.dart';
+import 'package:sentry/sentry.dart';
 import 'package:wasteagram/constants/constants.dart';
 import 'package:wasteagram/views/waste_post_list.dart';
 
 class App extends StatelessWidget {
+
+  // Sentry function logic is directly from the lecture on Sentry and crash reporting.
+  static Future<void> reportError(SentryClient sentry, dynamic error, dynamic stackTrace) async {
+    // Perform the default action in debug mode.
+    if (Foundation.kDebugMode) {
+      print(stackTrace);
+      return;
+    }
+
+    final SentryResponse response = await sentry.captureException(
+      exception: error,
+      stackTrace: stackTrace
+    );
+
+    if (response.isSuccessful) {
+      print('Sentry ID: ${response.eventId}');
+    }
+    else {
+      print('Failed to report to Sentry: ${response.error}');
+    }
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-import 'package:wasteagram/app.dart';
+import 'package:flutter/material.dart';
+import 'package:sentry/sentry.dart';
+
+import 'package:wasteagram/app.dart' as myApp;
 import 'package:wasteagram/components/counter_state_container.dart';
+import 'package:wasteagram/constants/reserved_constants.dart';
+
+final SentryClient sentry = SentryClient(dsn: ReservedConstants.sentryDSN);
 
 void main() {
-  runApp(CounterStateContainer(child: App()));
+  // From our lectures on Sentry and crash reporting
+  FlutterError.onError = (FlutterErrorDetails details) {
+    Zone.current.handleUncaughtError(details.exception, details.stack);
+  };
+  runZoned( () {
+    runApp(CounterStateContainer(child: myApp.App()));
+  }, onError: (error, stackTrace) {
+    myApp.App.reportError(sentry, error, stackTrace);
+  });
+  
 }
